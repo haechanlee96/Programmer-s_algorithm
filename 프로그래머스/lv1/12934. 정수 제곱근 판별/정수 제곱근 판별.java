1723e5bdb1851b9c1a17d8fdf1b3de28e6e27f72
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-06-20 09:19:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        int x = 0;
+        
+        
+        for (long i=0; i*i<=n; i++){
+            if((i*i)==n){
+                answer = (i+1)*(i+1);
+            } else {
+                answer = -1;
+            }
+        }
+        
+        return answer;
+    }
+}
