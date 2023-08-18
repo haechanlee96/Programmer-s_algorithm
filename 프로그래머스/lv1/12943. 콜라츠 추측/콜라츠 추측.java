ed3a1475279aec3cc7e09ea102cbbe3ff579c97b
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-08-18 13:49:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,27 +1,19 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
         int answer = 0;
-        int count = 0;
-        if(num == 1){
-            answer = 0;
-        }
         
-        while(num != 1){
+        while(num!=1){
             if(num%2==0){
                 num = num/2;
-                count++;
-            } else {
-                num = num*3 +1;
-                count++;
+            } else if(num%2==1){
+                num = (num*3) +1;
             }
+            answer++;
+            if(answer == 500){
+                answer = -1;
+                break;
         }
-            
-            answer = count;
-        if(answer > 500){
-            answer = -1;
-        }
-        
-        
+    }
         return answer;
     }
 }
