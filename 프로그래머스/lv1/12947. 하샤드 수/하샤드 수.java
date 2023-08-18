af7f6df6de3ff9d0e8f0b059242477bf2bd5b43b
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-08-18 11:43:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.11 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -2,13 +2,12 @@ class Solution {
     public boolean solution(int x) {
         boolean answer = false;
         
-        String str = Integer.toString(x);
-        String[] arr = str.split(""); 
+        String[] arr = String.valueOf(x).split("");
         
         int sum = 0;
         
-        for(int i = 0; i<arr.length; i++){
-            sum += Integer.parseInt(arr[i]);
+        for(String str : arr){
+            sum += Integer.parseInt(str);
         }
         
         answer = (x % sum ==0)? true : false;
