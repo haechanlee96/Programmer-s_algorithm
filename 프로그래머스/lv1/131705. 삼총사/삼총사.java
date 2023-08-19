895b5cb3113e1da3331efb70d6a205555129792b
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-08-19 13:52:31 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int[] number) {
-
-                int count = 0;
-                int len = number.length;
-                for (int i = 0; i < len - 2; i++) {
-                    for (int j = i + 1; j < len - 1; j++) {
-                        for (int k = j + 1; k < len; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        count++;
-                    }
+        int answer = 0;
+        
+        int sum = 0;
+        
+        for(int i =0; i<number.length; i++){
+            for(int j = i+1; j<number.length; j++){
+                for(int k = j+1; k<number.length; k++){
+                   sum = number[i] + number[j] + number[k];
+                    answer += (sum == 0)? 1 : 0;
                 }
             }
         }
         
-        return count;
+        return answer;
     }
 }
