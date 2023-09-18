f915fd51b5588b5ad0ca1eddd0a847e19677b25a
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-09-18 09:32:39 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         
-        List<String> temp = new ArrayList<>();
+       
         int index1 = 0;
         int index2 = 0;
         
         for (int i=0; i<goal.length; i++){
             if(index1<cards1.length && cards1[index1].equals(goal[i])){
-                temp.add(goal[i]);
+                
                 index1++;
                 
             } else if(index2 <cards2.length && cards2[index2].equals(goal[i])) {
-                temp.add(goal[i]);
+                
                 index2++;
             } else {
                 return "No";
